c90cc24f01e70938b997c98d5f810dad442f1d93
ropimple <generalthings999@gmail.com>
2024-08-06 13:07:31 +0530

    Adding prop Sig

---
+++ TrafficManagementSystem/SignalMode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficManagementSystem
+{
+    public class SignalMode
+    {
+        public bool SignalON  { get; set; }
+        public bool SignalOFF { get; set; }
+    }
+}
