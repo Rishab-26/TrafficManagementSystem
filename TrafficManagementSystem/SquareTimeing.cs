69fbc0dbd83a10d6038d5f8c6f0dad09991bd271
Ahush Litoriya <litoriyaayush@gmail.com>
2024-08-06 13:01:53 +0530

    class SquareTimeing Added and properties SqaareName,SignalTime Added By Ahush

---
+++ TrafficManagementSystem/SquareTimeing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficManagementSystem
+{
+    internal class SquareTimeing
+    {
+        public string SqaareName { get; set; }
+        public int SignalTime { get; set; }
+
+
+    }
+}
