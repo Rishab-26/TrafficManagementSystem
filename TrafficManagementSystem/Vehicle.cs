cfb42476f9f45573ec0e151a7823c871e1fe3941
Rishab Sharma <sharmarishab1998@gmail.com>
2024-08-06 13:05:06 +0530

    Added Class Vehicle by Rishab

---
+++ TrafficManagementSystem/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficManagementSystem
+{
+    internal class Vehicle
+    {
+        public string VehicleNo {  get; set; }
+        public string VehicleType { get; set; }
+        public string RCNumber { get; set; }
+
+        public string InsuranceNo { get; set; }
+    }
+}
