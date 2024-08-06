64f80bf95d9fe4a30cc8da945282a7886211d247
Vishal Wadekar <vishal-shashikant.wadekar@capgemini.com>
2024-08-06 12:57:23 +0530

    vishal created class "Challan", 3 property added

---
+++ TrafficManagementSystem/Challan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficManagementSystem
+{
+    public class Challan
+    {
+        public string VehicleNumber { get; set; }
+        public int ChallanAmount { get; set; }
+        public string ChallanStatus { get; set; }
+    }
+}
